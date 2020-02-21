372a002baac742eb8b07d4115b0780dcbb9e61ec
Felipe Azevedo <felipe.azevedo@adidas.com>
2020-02-21 15:29:37 +0100

    Adding the ability to load CSS also from the micro-frontend

--- src/components/MicroFrontend.js
+++ src/components/MicroFrontend.js
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { useCallback, useEffect } from 'react';
 
+function getStringID(name) {
+  return `${name}-script`;
+}
+
 const MicroFrontend = ({ name, host, history }) => {
   /**
    * Function used to render the microfrontend into the container.
@@ -21,24 +25,38 @@ const MicroFrontend = ({ name, host, history }) => {
   /**
    * Function used to add the microfrontend code into the container.
    */
-  const addScript = useCallback((scriptId, url, onload) => {
+  const addScript = useCallback((name, url, onload) => {
     const script = document.createElement('script');
-    script.setAttribute('id', scriptId);
+    script.setAttribute('id', getStringID(name));
     script.setAttribute('src', url);
     script.onload = onload;
     document.head.appendChild(script);
   }, []);
 
+  /**
+   * Function used to add the microfrontend style into the container.
+  */
+  const addStyle = useCallback((name, url) => {
+    const style = document.createElement('link');
+    style.setAttribute('id', `${name}-style`);
+    style.setAttribute('rel', 'stylesheet');
+    style.setAttribute('href', url);
+    document.head.appendChild(style);
+  }, []);
+
   useEffect(() => {
-    const scriptId = `micro-frontend-script-${name}`;
-    if (document.getElementById(scriptId)) {
+    const mfName = `micro-frontend-${name}`;
+    if (document.getElementById(getStringID(mfName))) {
       // if the microfrontend is already in the screen just render it
       renderMF();
     } else {
       // on the other hand we have to fetch the manifest and add the main.js bundle to the container
       fetch(`${host}/asset-manifest.json`)
         .then(res => res.json())
-        .then(manifest => addScript(scriptId, `${manifest['files']['main.js']}`, renderMF));
+        .then(manifest => {
+          addScript(mfName, `${manifest['files']['main.js']}`, renderMF);
+          addStyle(mfName, `${manifest['files']['main.css']}`);
+        });
     }
     return () => unmountMF();
   }, [host, name, renderMF, unmountMF, addScript]);
